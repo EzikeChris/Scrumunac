ae2009f20ffc1b3deeb441f1403f45a6d45b271e
Chris <christophermbe1009@gmail.com>
2022-06-09 16:41:31 +0100

    feat: MainSection Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from "./components/header/header";
 import HeroSection from "./components/Hero/hero";
+import Mainsection from "./components/mainsection/mainsection";
 import "./App.scss";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Header />
       <HeroSection />
+      <Mainsection />
     </div>
   );
 }
