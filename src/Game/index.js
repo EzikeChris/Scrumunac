fc533e8e93624d0ba18e6f635f42ec316f8d0291
Chris <christophermbe1009@gmail.com>
2022-06-09 22:50:02 +0100

    fix: Styled Main Section Image

---
+++ src/Game/index.js
@@ -0,0 +1,92 @@
+const GAME_DATA = [
+  {
+    id: 1,
+    title: "God Of War",
+    description: "God of War Ragnarok",
+    imageUrl:
+      "https://www.gematsu.com/wp-content/uploads/2021/09/God-of-War-Ragnarok_2021_09-09-21_007.png",
+    price: 20,
+  },
+  {
+    id: 2,
+    title: "God Of War",
+    description: "God of War Ragnarok",
+    imageUrl:
+      "https://www.gematsu.com/wp-content/uploads/2021/09/God-of-War-Ragnarok_2021_09-09-21_007.png",
+    price: 20,
+  },
+  {
+    id: 3,
+    title: "God Of War",
+    description: "God of War Ragnarok",
+    imageUrl:
+      "https://www.gematsu.com/wp-content/uploads/2021/09/God-of-War-Ragnarok_2021_09-09-21_007.png",
+    price: 20,
+  },
+  {
+    id: 4,
+    title: "God Of War",
+    description: "God of War Ragnarok",
+    imageUrl:
+      "https://www.gematsu.com/wp-content/uploads/2021/09/God-of-War-Ragnarok_2021_09-09-21_007.png",
+    price: 20,
+  },
+  {
+    id: 5,
+    title: "God Of War",
+    description: "God of War Ragnarok",
+    imageUrl:
+      "https://www.gematsu.com/wp-content/uploads/2021/09/God-of-War-Ragnarok_2021_09-09-21_007.png",
+    price: 20,
+  },
+  {
+    id: 6,
+    title: "God Of War",
+    description: "God of War Ragnarok",
+    imageUrl:
+      "https://www.gematsu.com/wp-content/uploads/2021/09/God-of-War-Ragnarok_2021_09-09-21_007.png",
+    price: 20,
+  },
+  {
+    id: 1,
+    title: "God Of War",
+    description: "God of War Ragnarok",
+    imageUrl:
+      "https://www.gematsu.com/wp-content/uploads/2021/09/God-of-War-Ragnarok_2021_09-09-21_007.png",
+    price: 20,
+  },
+  {
+    id: 7,
+    title: "God Of War",
+    description: "God of War Ragnarok",
+    imageUrl:
+      "https://www.gematsu.com/wp-content/uploads/2021/09/God-of-War-Ragnarok_2021_09-09-21_007.png",
+    price: 20,
+  },
+  {
+    id: 8,
+    title: "God Of War",
+    description: "God of War Ragnarok",
+    imageUrl:
+      "https://www.gematsu.com/wp-content/uploads/2021/09/God-of-War-Ragnarok_2021_09-09-21_007.png",
+    price: 20,
+  },
+  {
+    id: 9,
+    title: "God Of War",
+    description: "God of War Ragnarok",
+    imageUrl:
+      "https://www.gematsu.com/wp-content/uploads/2021/09/God-of-War-Ragnarok_2021_09-09-21_007.png",
+    price: 20,
+  },
+  {
+    id: 10,
+    title: "God Of War",
+    description: "God of War Ragnarok",
+    imageUrl:
+      "https://www.gematsu.com/wp-content/uploads/2021/09/God-of-War-Ragnarok_2021_09-09-21_007.png",
+    price: 20,
+  },
+];
+
+export default GAME_DATA;
--- src/components/mainsection/mainsection.jsx
+++ src/components/mainsection/mainsection.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
-import SpiderMan from "../../assets/Black-Ops-Cold-War-PS5-menu.webp";
+import SpiderMan from "../../assets/download.png";
 import "./mainsection.scss";
 
 function Mainsection({ history }) {
   return (
-    <div className="mainsection-container">
+    <div className="main-section-container">
       <div className="main-section-middle">
         <div className="ms-m-image">
           <img src={SpiderMan} alt="Spider-miles-morales" />
         </div>
         <div className="ms-m-description">
-          <h2> Swap Games at Cheap Rates</h2>
+          {/* <h2> Swap Games at Cheap Rates</h2>
           <p>
             We Make Owning games easy and cheap. Save more when you swap old
             games for new onces today.
-          </p>
+          </p> */}
           <button
             className="button is-black"
             id="shop-now"
